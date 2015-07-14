5c56e0fb6e2a4c236f8a8b44728345c34e3254ac
PButcher <pbutcher93@gmail.com>
2015-07-14 16:56:11 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// Debug mode?
+var debug = false;
+
+// Module to control application life.
+var app = require('app');
+
+// Module to create native browser window.
+var BrowserWindow = require('browser-window');
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+
+  // On OSX it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+
+    // Quit the application
+    app.quit();
+  }
+});
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  // Open the devtools.
+  if(debug) {
+    mainWindow.openDevTools();
+  }
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
