e94f30d70209a28aef262ad6fc06aefc533a9fa4
PButcher <pbutcher93@gmail.com>
2015-07-15 16:34:24 +0100

    Added entry screen

---
+++ js/entry.js
@@ -0,0 +1,7 @@
+// Load JQuery
+window.$ = window.jQuery = require('./vendor/jquery/dist/jquery.min.js');
+
+// jQuery magic happens here
+$("document").ready(function() {
+
+});
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Load JQuery
-window.$ = window.jQuery = require('./vendor/jquery/dist/jquery.min.js');
+window.$ = window.jQuery = require('../vendor/jquery/dist/jquery.min.js');
 
 // Load Blacksmith
-var blacksmith = require("./js/blacksmith.js");
+var blacksmith = require("../js/blacksmith.js");
 
 // jQuery magic happens here
 $("document").ready(function() {
