8c8167e612c2b82880471129a60fa08074183412
PButcher <pbutcher93@gmail.com>
2015-07-15 17:06:07 +0100

    Final touches to basic login and template for main

--- js/entry.js
+++ js/entry.js
@@ -4,4 +4,7 @@ window.$ = window.jQuery = require('./vendor/jquery/dist/jquery.min.js');
 // jQuery magic happens here
 $("document").ready(function() {
 
+  // Populate footer
+  $("footer").html("Built using io.js " + process.version +
+    " & Electron " + process.versions['electron']);
 });
